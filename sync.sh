4589b9a9720c5dbcd441620d9b34ce39aeac4db0
Ian Andwati <andwatiian@gmail.com>
2024-05-04 19:38:43 +0300

    added sync script

---
+++ sync.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+folder_path="~/GitHub/brave-traveler"
+
+cd "$folder_path"
+
+clean=$(git diff-index --quiet HEAD -- && git diff-files --quiet)
+
+if [ $clean -eq 0 ]
+then
+  echo "Working tree is clean. Skipping commit and pushing changes."
+  git push
+else
+  commit_message="$(date +'%A - %Y-%m-%d %H:%M:%S')"
+
+  git add .
+  git commit -m "$commit_message"
+
+  git push
+fi
